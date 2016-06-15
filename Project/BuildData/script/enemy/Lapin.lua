f2084e45c29466650803f9652bc5c56ab368cf01
kgtkr <kgtkr.jp+program@gmail.com>
2016-06-15 20:06:50 +0900

    ラパン

---
+++ Project/BuildData/script/enemy/Lapin.lua
@@ -0,0 +1,41 @@
+--[[
+NAME=ラパン
+HP=1500
+IMAGE=resource/lapin.png
+R=15
+CLASS=Lapin
+]]
+
+package.path =  "script/lib/?.lua";
+require("cmd");
+
+Lapin={
+    new=function(api)
+        local this={
+            api=api,
+            sync=cmd.Sync.new()
+        };
+
+        return setmetatable(this, {__index = Lapin}); 
+    end,
+
+    update=function(this)
+        if not this.sync:isNeed() then
+            table.insert(this.sync.list,cmd.Charge.new(this.api,this.api.Image.Charge,180,this.api.R*3,255,0,0));
+            table.insert(this.sync.list,cmd.Action.new(function()this.api.Power=40; end));
+            for i = 1, 10 do
+                table.insert(this.sync.list,cmd.ULMPlayer.new(this.api,20,cmd.retFunc(10)));
+            end
+            table.insert(this.sync.list,cmd.Action.new(function()this.api.Power=0; end));
+        end
+        this.sync:update();
+    end,
+
+    draw=function(this)
+        this.sync:draw();
+        this.api:Draw();
+    end,
+
+    dispose=function(this)
+    end,
+};
